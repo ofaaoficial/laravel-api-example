fdf0628d85c4052b2ca81687d04f0df1613354e0
ofaaoficial <oscarfamado@gmail.com>
2019-03-23 11:17:45 -0500

    se hacen Test [UserTest] al sistema de autenticacion y registro de usuarios

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,27 +2,45 @@
 
 namespace Tests\Feature;
 
+use Faker\Factory;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function can_register(){
-        $data = [
-            'name' => 'test',
-            'username' => 'test',
-            'email' => 'test@test.test',
+    function test_can_register(){
+
+        $faker = Factory::create();
+        $username = $faker->userName;
+
+        $this->post('/api/register',[
+            'name' => $faker->name,
+            'username' => $username,
+            'email' => $faker->safeEmail,
             'password' => bcrypt('test'),
-            'token' => md5('test'),
-        ];
-
-        $this->post('/api/register',$data)
-//            ->assertJson($data)
+            'token' => md5($username),
+        ])
+            ->assertJson([
+                'message' => 'Usuario registrado correctamente.'
+            ])
             ->assertStatus(201);
+    }
 
+    function test_can_login(){
+        $this->post('api/login', [
+            'username' => 'ofaaoficial',
+            'password' => 'ofaaoficial'
+        ])
+            ->assertJson([
+                'message' => 'Ingreso satisfactorio.'
+            ])
+            ->assertStatus(200);
+    }
+
+    function test_can_logout(){
+        $this->get('api/logout',['Authorization' => "Bearer 14c4b06b824ec593239362517f538b29"])
+            ->assertJson([
+                "message" => "Cierre de sesion satisfactorio."
+            ])
+            ->assertStatus(200);
     }
 }
